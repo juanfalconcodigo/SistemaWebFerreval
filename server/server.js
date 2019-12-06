93908f05878dd49021e00811b73829fa3fabaf04
Juan Falcon <juancanen_10_@hotmail.com>
2019-12-06 18:05:38 -0500

    Implementación de Cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const colors = require('colors/safe');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 //Acceso a la configuración
 require("./config.js");
@@ -11,12 +12,22 @@ mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useUnifiedTopology:
 }).catch(console.err);
 
 
-
+//para el problema de cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+/* app.use(cors()); */
 
 //Configurando middlewares
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(require('./routes/index'));
+
 
 
 
